10dc2509e5db2465329ebdee6f870ddd52fa37d5
anesashylemaja <as72966@ubt-uni.net>
2025-12-26 15:49:34 +0100

    Login validimi

---
+++ login.js
@@ -0,0 +1,59 @@
+let user = document.getElementById('user');
+let psw = document.getElementById('password');
+
+user.addEventListener('input', function() {
+    document.getElementById('mesazhi1').innerText = '';
+    document.getElementById('mesazhi').innerText = '';
+});
+
+psw.addEventListener('input', function() {
+    document.getElementById('mesazhi2').innerText = '';
+    document.getElementById('mesazhi').innerText = '';
+});
+
+function Validate(){
+    let user =document.getElementById('user');
+    let psw = document.getElementById('password');
+    let errors=null;
+    let errorsUser=null;
+    let errorsPass=null;
+    let error1=null;
+    let regex1=/^[a-zA-Z0-9._-]{6,}$/;
+    let regex2=/^(?=.*[\d])(?=.*[!@#$%])[a-zA-Z\d!@#$%]{8,}$/;
+
+    if(user.value==""){
+        errorsUser='Please enter your username';
+        error1=user;
+    }else if(!(regex1.test(user.value))){
+        errorsUser='Username must be atleast 6 characters';
+        error1=user;
+    } if(psw.value==""){
+         errorsPass='Please enter your password';
+        error1=psw;
+    }else if(!(regex2.test(psw.value))){
+         errorsPass='Password must be atleast 12 characters, 1 symbol and 1 digit';
+        error1=psw;
+    }
+    if(errorsUser!=null & errorsPass!=null){
+        errors=errorsUser+ " and "+errorsPass;
+        var mesazhi = document.getElementById('mesazhi').innerText=errors;
+        error1=user;
+        error1.focus();
+        return false;
+    }else if(errorsUser!=null){
+         var mesazhi1 = document.getElementById('mesazhi1').innerText=errorsUser;
+          error1.focus();
+        return false;
+    }else{
+         var mesazhi2 = document.getElementById('mesazhi2').innerText=errorsPass;
+          error1.focus();
+        return false;
+    }
+    /* if(errors!=null){
+        var mesazhi= document.getElementById('mesazhi').innerText=errors;
+        error1.focus();
+        return false;
+    } */
+
+    return true;
+}
